390100fe551b4c6993056a8e32792d393b41ba8b
healerzhou <17816611897@163.com>
2018-11-30 15:03:11 +0800

    demo2

---
+++ src/main/java/com/healerzhou/Application.java
@@ -0,0 +1,17 @@
+package com.healerzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @Description
+ * @Author zl
+ * @Date 2018-11-30
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
